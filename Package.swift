4799a66cc0f8577dc72f69944cbceb3ac3b417c7
Marius Pătru <marius.patru@gmail.com>
2022-09-28 18:27:33 +0200

    Add `.static` and `.dynamic` products.

--- Package.swift
+++ Package.swift
@@ -8,12 +8,35 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
+        // Auto
         .library(
             name: "Lasso",
             targets: ["Lasso"]
         ),
         .library(
             name: "LassoTestUtilities",
+            targets: ["LassoTestUtilities"]
+        ),
+        // Static
+        .library(
+            name: "LassoStatic",
+            type: .static,
+            targets: ["Lasso"]
+        ),
+        .library(
+            name: "LassoTestUtilitiesStatic",
+            type: .static,
+            targets: ["LassoTestUtilities"]
+        ),
+        // Dynamic
+        .library(
+            name: "LassoDynamic",
+            type: .dynamic,
+            targets: ["Lasso"]
+        ),
+        .library(
+            name: "LassoTestUtilitiesDynamic",
+            type: .dynamic,
             targets: ["LassoTestUtilities"]
         )
     ],
